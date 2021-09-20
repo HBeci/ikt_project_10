e10bdfd727ec6fa6b175dda30d604db4aecbe290
HBeci <90601595+HBeci@users.noreply.github.com>
2021-09-20 10:34:24 +0200

    Add files via upload

---
+++ 1recept.py
@@ -0,0 +1,10 @@
+def csirkerizzsel(B_type, side_dish):
+    print('Főzd meg a rizst addig amíg nem puha.')
+    print('vágd kis szeletekre a húst')
+    print('fűszerezd meg')
+    print('süsd meg')
+    print('vágj fel mellé' + B_type + '-ot.')
+    print('iggyá mellé' + side_dish + '-t.')
+
+csirkerizzsel('paradicsom', ' kóla')
+
