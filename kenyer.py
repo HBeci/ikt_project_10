28968be72092b21ff1ab68bd6555cf0fbb905111
HBeci <90601595+HBeci@users.noreply.github.com>
2021-09-26 18:53:21 +0200

    Add files via upload

---
+++ kenyer.py
@@ -0,0 +1,9 @@
+def lekvároskenyér(type, dish):
+    print('kend meg a kenyeret vajjal.')
+    print('azután pedig...')
+    print('lekvárall')
+    print('lehet' + type + '-ral.')
+    print('vagy' + dish + '-ral.')
+    print('jó étvágyat')
+
+lekvároskenyér(' baracklekvár', ' eperlekvár')
