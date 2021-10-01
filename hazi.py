122377d8aa7b7797477e5683c16f57d89abc75e8
HBeci <90601595+HBeci@users.noreply.github.com>
2021-10-01 13:03:00 +0200

    Add files via upload

---
+++ hazi.py
@@ -0,0 +1,11 @@
+allat = input('milyen fajta autód van?   ')
+neve = input('diesel vagy benzin ?   ')
+szin = input('milyen színü ?    ')
+eves = float(input('mennyit eszik 100-on ? l=  '))
+mekkora = int(input('hány éves ?    '))
+
+
+
+
+print('neked van egy',allat,'-od/-d ami',neve,'-es ,és',szin,'színü.',eves,'l fogyaszt 100-on és ',mekkora,'éves .')
+
