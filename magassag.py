164cd0af7aba4fc1f2f58f42761637560a5c11c0
HBeci <90601595+HBeci@users.noreply.github.com>
2021-10-04 19:50:18 +0200

    Add files via upload

---
+++ magassag.py
@@ -0,0 +1,11 @@
+height = int(input('milyen magas vagy ? cm: '))
+
+if (height > 180):
+  print('te zsiráf.')
+elif (height < 170):
+    print('te törpe.')
+else :
+    print('átlagos magasságu vagy')
+
+
+
