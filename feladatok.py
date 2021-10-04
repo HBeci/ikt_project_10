a1335ab9a8d062e5669c0d8e8e9a43ecbfc0c014
HBeci <90601595+HBeci@users.noreply.github.com>
2021-10-04 09:58:31 +0200

    Add files via upload

---
+++ feladatok.py
@@ -0,0 +1,17 @@
+name = input('mi a teljes neved?   ')
+print('Hello',name,' Legyen szép napod.')
+
+print('*******')
+print('ooooooo')
+
+print('*o*o*o*o*o')
+print('*o*o*o*o*o')
+
+szam1 = int(input('adj meg egy számot:  '))
+szam2 = int(input('adj meg egy másik számot:  '))
+print('két szám összege:',szam1 + szam2,)
+
+szam1 = int(input('adj meg egy számot:  '))
+szam2 = int(input('adj meg egy másik számot:  '))
+szam3 = int(input('adj meg még egy számot:  '))
+print('két szám összege:',szam1 + szam2 + szam3,)
