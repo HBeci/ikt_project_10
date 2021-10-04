f218c42d43d07ea500321f92a1f1e0922cddb060
HBeci <90601595+HBeci@users.noreply.github.com>
2021-10-04 10:01:26 +0200

    Update feladatok.py

--- feladatok.py
+++ feladatok.py
@@ -1,17 +1,29 @@
+print('feladat1')
+
 name = input('mi a teljes neved?   ')
 print('Hello',name,' Legyen szép napod.')
+
+print('feladat2')
 
 print('*******')
 print('ooooooo')
 
+print('feladat3')
+
 print('*o*o*o*o*o')
 print('*o*o*o*o*o')
+
+print('feladat3')
 
 szam1 = int(input('adj meg egy számot:  '))
 szam2 = int(input('adj meg egy másik számot:  '))
 print('két szám összege:',szam1 + szam2,)
 
+print('feladat4')
+
 szam1 = int(input('adj meg egy számot:  '))
 szam2 = int(input('adj meg egy másik számot:  '))
 szam3 = int(input('adj meg még egy számot:  '))
 print('két szám összege:',szam1 + szam2 + szam3,)
+
+print('feladat5')
