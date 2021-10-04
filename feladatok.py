391ce4a2759c55271539b5dc79548a5210787866
HBeci <90601595+HBeci@users.noreply.github.com>
2021-10-04 19:41:39 +0200

    Update feladatok.py

--- feladatok.py
+++ feladatok.py
@@ -26,4 +26,24 @@ szam2 = int(input('adj meg egy másik számot:  '))
 szam3 = int(input('adj meg még egy számot:  '))
 print('két szám összege:',szam1 + szam2 + szam3,)
 
+print('feladat6')
+
+szam1 = int(input('adj meg egy számot:  '))
+szam2 = int(input('adj meg egy másik számot:  '))
+print('két szám összege:',szam1 - szam2,)
+
+print('feladat7')
+
+szam1 = int(input('adj meg egy számot:  '))
+szam2 = int(input('adj meg egy másik számot:  '))
+print('két szám összege:',szam1 * szam2,)
+
+print('feladat8')
+
+szam1 = int(input('adj meg egy számot:  '))
+szam2 = int(input('adj meg egy másik számot:  '))
+print('két szám összege:',szam1 / szam2,)
+
+
+
 
