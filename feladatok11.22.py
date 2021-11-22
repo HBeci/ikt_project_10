36033563654b67de661e4adffdf81c021961e0d9
HBeci <90601595+HBeci@users.noreply.github.com>
2021-11-22 10:32:24 +0100

    Add files via upload

---
+++ feladatok11.22.py
@@ -0,0 +1,75 @@
+for i in range(0,11,2):
+    print(i)
+
+#feladat 13
+
+for x in range(0,101,3):
+    print(x)
+
+#feladat 14
+
+number = int(input('adj meg egy számot '))
+print ('a szám kétszerese:',number*2,)
+
+#feladat 15
+
+print(0+1+2+3+4)
+
+#feladat 16
+
+for numb in range(0,11):
+    print('1')
+    print('4')
+    print('9')
+    print('16')
+    print('25')
+    print('36')
+    print('49')
+    print('64')
+    print('81')
+    print('100')
+
+#feladat 17
+
+for kugya in range(0,31):
+    print(kugya*kugya)
+
+#feladat 18
+for num in range(0,101,2):
+    print(num)
+
+#feladat 19
+
+number1 = int(input('adj meg egy számot  '))
+number2 = int(input('adj meg egy másik számot  '))
+print(number1*number1)
+print(number2*number2)
+
+#feladat 20
+
+for fifty in range(50,10,-2):
+    print(fifty)
+
+#feladat 21
+
+negyzet = int(input('adj meg egy számot  '))
+print ('K=',4*negyzet,)
+print ('T=',negyzet*negyzet,)
+
+#feladat 22
+
+teglalapA = int(input('adj meg egy számot  '))
+teglalapB = int(input('adj meg egy számot  '))
+Kerulet = print(teglalapA + teglalapB)
+print ('k=',Kerulet*2)
+print ('t=',teglalapA*teglalapB,)
+
+#feladat 23
+
+print(1+2+3+4+5+6+7+8+9+10)
+
+#feladat 24
+
+for nyolc in range(80,20,-1):
+    print(nyolc)
+
